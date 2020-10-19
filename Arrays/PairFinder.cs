01acda9c3a38530652a659ee43058f28c4807a63
Abdallah <tahboub252@gmail.com>
2020-10-19 23:52:14 +0200

    Implemented pairFinder to find pairs in array

---
+++ Arrays/PairFinder.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace data_structure_and_algorithms
+
+{
+     class PairFinder {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sum"></param>
+        /// <param name="arrayLength"></param>
+        public static void FindPair(int sum, int arrayLength)
+
+        {
+            /// <summary>
+            /// Counting the pairs in the array if found.
+            /// </summary>
+            int counter = 0;
+
+            /// <summary>
+            /// allocating the array with numbers generated randomly.
+            /// </summary>
+            /// <value>Random values</value>
+             Random r = new Random ();
+            int[] myAarray = new int[arrayLength];
+            for (int i = 0; i < myAarray.Length - 1; i++) {
+
+                var random = r.Next(-100, 100);
+                myAarray[i] = random;
+                Console.WriteLine (myAarray[i] + " " + "i" + "=" + i);
+
+            }
+
+            for (int i = 0; i < myAarray.Length - 1; i++)
+            {
+                // i+1 for no replications. //Optimaization.
+
+                for (int s = i + 1; s < myAarray.Length;s++) {
+
+                    if (myAarray[i] + myAarray[s] == sum) {
+
+                        counter++;
+
+                        Console.WriteLine ("Pair found at " + i + "" + "and" + "" + s + ":");
+                        /// <summary>
+                        /// Break for optimization. Otherwise it would take for ever. 
+                        /// </summary>
+                        break;
+
+                    } else {
+                        Console.WriteLine("No Pair found at " + i + "" + "and" + "" + s + ":");
+                        /// <summary>
+                        /// Break for optimization. Otherwise it would take for ever. 
+                        /// </summary>
+
+                        break;
+
+                    }
+
+
+                }
+
+            }
+
+            Console.WriteLine(counter + " " + "pairs found with the sum" + "" + " " + sum);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace data_structure_and_algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            PairFinder.FindPair(4, 1000000);
         }
     }
 }
