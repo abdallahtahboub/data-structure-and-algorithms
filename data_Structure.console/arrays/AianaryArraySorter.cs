65549486f757182d93564bd48f7d872851b91d7b
Abdallah Tahboub <tahboub252@gmail.com>
2021-05-16 22:02:33 +0200

    Refactoring

--- data_Structure.console/arrays/AianaryArraySorter.cs
+++ data_Structure.console/arrays/AianaryArraySorter.cs
@@ -7,27 +7,16 @@ namespace data_structure_and_algorithms
     public static class BinaryArraySort
     {
 
-        static int[] MyArray;
+
 
         /// <summary>
         /// Sorting a binary array in a linear time o(n)  Linear Complexity.
         /// </summary>
         /// <param name="arrayLength"></param>
-        public static int SortBinaryArray(int[] MyArray, int length)
+        public static int SortBinaryArray(int[] MyArray, int arrayLength)
         {
 
-            //int[] myArray = new int[arrayLength];
-            // Random r = new Random();
-
-            //     for (int i = 0; i<myArray.Length; i++)
-            //     {
-
-            //         var random = r.Next(0, 2);
-            // myArray[i] = random;
-            //         Console.WriteLine(myArray[i] + " " + "i" + "=" + i);
-
-            //     }
-
+           int[] myArray = ArrayAllocator.AllocateArrayRandomly(arrayLength);
 
             int zeroCounter = 0;
             for (int i = 0; i < MyArray.Length; i++)
